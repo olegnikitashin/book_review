72c9e84bb46ce16ba1da6f6522c04903ed4f86f2
Oleg Nikitashin <olegnikitashin@gmail.com>
2016-06-18 02:15:04 +0300

    Complete Raty JS function, correct the reviews form views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :find_book
   before_action :find_review, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
 
   def new
     @review = Review.new
